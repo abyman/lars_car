c86ca201e6abff2f5d115672f414a5e60c7dfc52
aaron <aaron.byman@gmail.com>
2015-11-25 21:10:35 +0200

    Added light control

--- motors.py
+++ motors.py
@@ -45,6 +45,10 @@ def setServoAngle(s,pwm,pwmNum=15):
    sp = ((SERVO_LEFT+SERVO_RIGHT)/2) + s*(SERVO_LEFT-SERVO_RIGHT)/2
    accel.adjustPwmOnTime(pwm,pwmNum,PWM_HZ,sp,0.4)
    
+def setLightIntensity(sp,pwm,pwmNum=8):
+   if sp <= 1 and sp >= 0:
+      accel.adjustPwmDuty(pwm,pwmNum,sp,0.0)
+   
 def usage():
    print("Enter one of the following commands with associated parameter")
    print(" a <speed> // motor a will turn at speed. speed is (-1:1)")
@@ -52,20 +56,26 @@ def usage():
    print(" c <speed> // motor a&b will turn at speed. speed is (-1:1)")
    print(" s <pos> // servo s will turn to pos. pos is (-1:1)")
    print(" t <delay> // servo s will turn right and then left. each step will be delayed by delay")
+   print(" l <num> <intensity> // light <num (1:2)> on with <intensity (0:1)>")
    print(" exit // exit this prog")
 
 s = 0
 setServoAngle(s,pwm)
 setMotorSpeed(0,pwm,[0,1])      
 setMotorSpeed(0,pwm,[2,3])      
+setLightIntensity(0.015,pwm,12)
+setLightIntensity(0.015,pwm,13)
 
 if len(sys.argv) > 1:
    spl = sys.argv[1:]
-   if spl[0] == 'a' or spl[0] == 'b' or spl[0] == 'c' or spl[0] == 's' or spl[0] == 't':
+   if spl[0] == 'a' or spl[0] == 'b' or spl[0] == 'c' or spl[0] == 's' or spl[0] == 't' or spl[0] == 'l':
       if len(spl) < 2:
          usage()
       else:
-         v = float(spl[1])
+         if len(spl) > 2:
+            v = float(spl[2])
+         else:
+            v = float(spl[1])
          if v < -1 or v > 1:
             usage()
          else:
@@ -85,6 +95,9 @@ if len(sys.argv) > 1:
                for a in range(-16,1,1):
                   setServoAngle(a*0.01,pwm)
                   time.sleep(v)
+            if spl[0] == 'l':
+               setLightIntensity(v,pwm,int(spl[1])+11)
+               print "%s, %s" % (v, int(spl[1]))
    elif spl[0] == 'exit':
       run = 0
    else:
@@ -97,11 +110,14 @@ else:
          usage()
       else:
          spl = string.split(string.lower(raw))
-         if spl[0] == 'a' or spl[0] == 'b' or spl[0] == 'c' or spl[0] == 's':
+         if spl[0] == 'a' or spl[0] == 'b' or spl[0] == 'c' or spl[0] == 's' or spl[0] == 't' or spl[0] == 'l':
             if len(spl) < 2:
                usage()
             else:
-               v = float(spl[1])
+               if len(spl) > 2:
+                  v = float(spl[2])
+               else:
+                  v = float(spl[1])
                if v < -1 or v > 1:
                   usage()
                else:
@@ -111,6 +127,18 @@ else:
                      setMotorSpeed(v,pwm,[2,3])
                   if spl[0] == 's':
                      setServoAngle(v,pwm)
+                  if spl[0] == 't':
+                     for a in range(0,17,1):
+                        setServoAngle(a*0.01,pwm)
+                        time.sleep(v)
+                     for a in range(16,-17,-1):
+                        setServoAngle(a*0.01,pwm)
+                        time.sleep(v)
+                     for a in range(-16,1,1):
+                        setServoAngle(a*0.01,pwm)
+                        time.sleep(v)
+                  if spl[0] == 'l':
+                     setLightIntensity(v,pwm,int(spl[1])+7)
          elif spl[0] == 'exit':
             run = 0
          else:
